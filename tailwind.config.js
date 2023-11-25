8c11d69b5ca90e18f526ef6c9b54aaea3a71cec6
Bere-Avila <bere0664@gmail.com>
2023-11-25 01:18:26 -0600

    Definición de paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
   export default {
     content: ['./src/views/**/*.pug',],
     theme: {
+      extend:{
+        colors:{
+          'black': '000000',
+          'oxford blue': '14213D',
+          'orange': 'FCA311',
+          'platinum':'E5E5E5',
+          'white':'FFFFFF',
+          'xanthous':'EAB01F',
+          'ochre':'CC750D',
+          'russet':'844F29',
+          'rsisin black': '2D2C36',
+          'rich black': '12121C'
+        },
+      },
     },
     plugins: [],
   }
