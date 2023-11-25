8f827bdb17fb4029f392f17151788dc49f960b0b
PpGrillo1511 <jlozamarin@gmail.com>
2023-11-25 01:45:53 -0600

    colors register

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@
     theme: {
       extend:{
         colors:{
-          'black': '000000',
-          'oxford blue': '14213D',
-          'orange': 'FCA311',
-          'platinum':'E5E5E5',
-          'white':'FFFFFF',
-          'xanthous':'EAB01F',
-          'ochre':'CC750D',
-          'russet':'844F29',
-          'rsisin black': '2D2C36',
-          'rich black': '12121C'
+          'black': '#000000',
+          'oxford blue': '#14213D',
+          'orange': '#FCA311',
+          'platinum':'#E5E5E5',
+          'white':'#FFFFFF',
+          'xanthous':'#EAB01F',
+          'ochre':'#CC750D',
+          'russet':'#844F29',
+          'rsisin-black': '#2D2C36',
+          'rich-black': '#12121C'
         },
       },
     },
